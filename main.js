a23d05434ece096cbbce6fca533f7f3eacc00b6f
belenPadilla <padillabelenrosario@gmail.com>
2022-11-16 20:21:02 -0300

    hice nuevos arregllos, agregue toastify

--- main.js
+++ main.js
@@ -9,42 +9,33 @@ constructor( id,nombre, precio,img,cantidad){
 }
 }
 
-const calzaanimalblack = new Producto (1,"AnimalBlack", 3500, "fotosUmmaReal/calzayremeblondie.jpg");
-const calzaCamuflada = new Producto(2,"Camuflada", 3000, "fotosUmmaReal/calzacamuflada.jpg" );
-const calzaLeopardoB = new Producto(3,"leopardo Black", 4200,"fotosUmmaReal/animalblack.jpg");
-const calzaDurazno = new Producto(4, "Texturizada Durazno", 4200,"fotosUmmaReal/calzadurazno.jpg");
-const calzaClv = new Producto(5,"calza CLV", 4200,"fotosUmmaReal/celesterosa.jpg");
-const calzaGrafito = new Producto(6,"Grafito", 4200,"fotosUmmaReal/grisgrafito.jpg");
-const calzaStart = new Producto(7,"Start Black", 5700,"fotosUmmaReal/calzastartblack.jpg");
-const calzaLeopardoF = new Producto(8,"Leopardo Fucsia", 4200,"fotosUmmaReal/animalrosada.jpg");
-const biker1 = new Producto(9, "Biker Print",3500,"fotosUmmaReal/bikeranimaladelante.jpg");
-const biker2 = new Producto(10, "Biker Animal", 3500,"fotosUmmaReal/bikerprintgrisadelante.jpg");
-const biker3 = new Producto(11,"Biker Rosa",4200,"fotosUmmaReal/bikerprintmarronadelante.jpg");
-const pescadora1 = new Producto(12, "Pescadora Pasteles", 4200,"fotosUmmaReal/pescadorapastelesadelante.jpg");
-const pescadora2 = new Producto(13," pescadora b&w", 4200,"fotosUmmaReal/calzapesb&w.jpg");
-const short1 = new Producto(14, "Shor Caricaturas", 3500,"fotosUmmaReal/shortcaricaturaatras.jpg");
-const short2 = new Producto(15, "short flores pateles", 3500,"fotosUmmaReal/shortflores222.jpg");
-const short3 = new Producto(16, "short humo", 3500,"fotosUmmaReal/shorthumoyremeadelante.jpg");
-const top1 = new Producto(17, "yoga", 3500,"fotosUmmaReal/remeyshortconjuntoAzul.jpg");
-const top2 = new Producto(18, "Conjunto Red", 3500,"fotosUmmaReal/conjuntoNuevored.jpg");
-const top3 = new Producto(19, "encofrado rosa", 3200,"fotosUmmaReal/toprosaencofrado.jpg");
-const top4 = new Producto(20, "MAMBO",3200,"fotosUmmaReal/topnegroair.jpg");
-const top5 = new Producto(21, "top bloondlie", 3500,"fotosUmmaReal/topanimaladelante.jpg");
-const top6 = new Producto(22, "encofrado lila", 3200,"fotosUmmaReal/topencofradolila.jpg");
-const top7 = new Producto(23, "ecofrado fit",3200,"fotosUmmaReal/topencofradoceleste.jpg");
-const top8 = new Producto(24, "Vibora", 3500,"fotosUmmaReal/topviboraadelante.jpg");
-const remera1 = new Producto(25,"crotop azul",1600,"fotosUmmaReal/crotopazul.jpg");
-const remera2 = new Producto(26, "crotop bordo",1600,"fotosUmmaReal/crotopbordo.jpg");
-const remera3 = new Producto(27,"crotop rosa",1600,"fotosUmmaReal/crotoprosa.jpg");
-const remera4 = new Producto(28,"remera carita",1800,"fotosUmmaReal/remecaritaadelante.jpg");
-const remera5 = new Producto(29,"musculosa Air wind",3200 ,"fotosUmmaReal/musconegraadelante.jpg");
-const remera6 = new Producto(30,"musculosa basica blanca",1800,"fotosUmmaReal/muscublanca.jpg");
-const remera7 = new Producto(31,"musculosa basica beige",1800,"fotosUmmaReal/muscubeige.jpg");
-const remera8 = new Producto(32,"musculosa Air wind gris",3200,"fotosUmmaReal/airdwind.jpg");
+
+const calzaLeopardoB = new Producto(1,"leopardo Black", 4200,"fotosUmmaReal/animalblack.jpg");
+const calzaDurazno = new Producto(2, "Texturizada Durazno", 4200,"fotosUmmaReal/calzadurazno.jpg");
+const calzaClv = new Producto(3,"calza CLV", 4200,"fotosUmmaReal/celesterosa.jpg");
+const calzaGrafito = new Producto(4,"Grafito", 4200,"fotosUmmaReal/grisgrafito.jpg");
+const calzaStart = new Producto(5,"Start Black", 5700,"fotosUmmaReal/calzastartblack.jpg");
+const calzaLeopardoF = new Producto(6,"Leopardo Fucsia", 4200,"fotosUmmaReal/animalrosada.jpg");
+const biker1 = new Producto(7, "Biker Print",3500,"fotosUmmaReal/bikeranimaladelante.jpg");
+const biker2 = new Producto(8, "Biker Animal", 3500,"fotosUmmaReal/bikerprintgrisadelante.jpg");
+const biker3 = new Producto(9,"Biker Rosa",4200,"fotosUmmaReal/bikerprintmarronadelante.jpg");
+const pescadora1 = new Producto(10, "Pescadora Pasteles", 4200,"fotosUmmaReal/pescadorapastelesadelante.jpg");
+const pescadora2 = new Producto(11," pescadora b&w", 4200,"fotosUmmaReal/calzapesb&w.jpg");
+const short1 = new Producto(12, "Shor Caricaturas", 3500,"fotosUmmaReal/shortcaricaturaatras.jpg");
+const short2 = new Producto(13, "short flores pateles", 3500,"fotosUmmaReal/shortflores222.jpg");
+const short3 = new Producto(14, "short humo", 3500,"fotosUmmaReal/shorthumoyremeadelante.jpg");
+const top1 = new Producto(15, "yoga", 3500,"fotosUmmaReal/remeyshortconjuntoAzul.jpg");
+const top2 = new Producto(16, "Conjunto Red", 3500,"fotosUmmaReal/conjuntoNuevored.jpg");
+const top3 = new Producto(17, "encofrado rosa", 3200,"fotosUmmaReal/toprosaencofrado.jpg");
+const top4 = new Producto(18, "MAMBO",3200,"fotosUmmaReal/topnegroair.jpg");
+const top5 = new Producto(19, "top bloondlie", 3500,"fotosUmmaReal/topanimaladelante.jpg");
+const top6 = new Producto(20, "encofrado lila", 3200,"fotosUmmaReal/topencofradolila.jpg");
+const remera1 = new Producto(21,"crotop azul",1600,"fotosUmmaReal/crotopazul.jpg");
+const remera2 = new Producto(22, "crotop bordo",1600,"fotosUmmaReal/crotopbordo.jpg");
+const remera3 = new Producto(23,"crotop rosa",1600,"fotosUmmaReal/crotoprosa.jpg");
+const remera4 = new Producto(24,"remera carita",1800,"fotosUmmaReal/remecaritaadelante.jpg");
 
 const productos = [
-    calzaanimalblack,
-    calzaCamuflada,
     calzaLeopardoB,
     calzaDurazno,
     calzaClv,
@@ -65,16 +56,10 @@ const productos = [
     top4,
     top5,
     top6,
-    top7,
-    top8,
     remera1,
     remera2,
     remera3,
-    remera4,
-    remera5,
-    remera6,
-    remera7,
-    remera8
+    remera4
 ];
 
 let carrito = [];
@@ -117,6 +102,7 @@ const agregarAlCarrito = (id) => {
     calcularCompra();
 }
 
+
 verProductos();
 
 const divCarrito = document.getElementById("divCarrito");
@@ -140,14 +126,14 @@ const mostrarCarrito = () => {
         <p class="card-text">${Producto.precio}</p>
         <p class="card-text">${Producto.cantidad}</p>
         <button class="btn colorBotones" id="eliminar${Producto.id}">Eliminar</button>
-        </div>
+    </div>
     </div>`
 
     divCarrito.appendChild(tarjeta);
 
     const boton = document.getElementById(`eliminar${Producto.id}`);
     boton.addEventListener("click", () => {
-    eliminarProducto(Producto.id);
+    eliminarProducto(Producto.id)
     })
 })
     calcularCompra();
@@ -185,3 +171,12 @@ const calcularCompra = () => {
 }
 
 const jsonProductos = JSON.stringify(productos);
+
+vaciarCarrito.addEventListener("click",()=>{
+    Toastify({
+        text: "Su carrito esta vacio",
+        duration: 3500,
+        gravity: "top",
+        position:"left",
+    }).showToast();
+})
